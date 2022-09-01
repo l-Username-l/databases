84669d0772b223b75e6eb200c598361ad1997cc2
l-Username-l <zhmyhun@gmail.com>
2022-09-01 14:03:24 +0900

    commit

---
+++ Programmers/3. GROUP BY/1. 고양이와 개는 몇 마리 있을까.sql
@@ -0,0 +1,7 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59040
+
+-- 동물 보호소에 들어온 동물 중 고양이와 개가 각각 몇 마리인지 조회
+
+SELECT ANIMAL_TYPE, COUNT(ANIMAL_TYPE) AS count FROM ANIMAL_INS
+GROUP BY ANIMAL_TYPE
+ORDER BY ANIMAL_TYPE
