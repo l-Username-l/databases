72f5cd175252a8821df0a922ae187896d208de30
l-Username-l <zhmyhun@gmail.com>
2022-09-02 18:03:46 +0900

    commit

--- Java_MySQL/src/ConnectMySQL.java
+++ Java_MySQL/src/ConnectMySQL.java
@@ -1,5 +1,28 @@
-public class ConnectMySQL {
-    public static void main(String[] args){
+import java.sql.*;
 
-    }
+public class ConnectMySQL {
+   static final String DB_URL = "jdbc:mysql://localhost/world";
+   static final String USER = "root";
+   static final String PASS = "tbrs00002b";
+   static final String QUERY = "select ID, NAME, CountryCode, district, population " + "from 도식1";
+
+   public static void main(String[] args) {
+      // Open a connection
+      try{
+        Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+         Statement stmt = conn.createStatement();
+         ResultSet rs = stmt.executeQuery(QUERY);
+         // Extract data from result set
+         while (rs.next()) {
+            // Retrieve by column name
+            System.out.print("ID: " + rs.getString("id"));
+            System.out.print(", Age: " + rs.getString("name"));
+            System.out.print(", First: " + rs.getString("countrycode"));
+            System.out.print(", Last: " + rs.getString("district"));
+            System.out.println(", First: " + rs.getInt("population"));
+         }
+      } catch (Exception e) {
+         e.printStackTrace();
+      } 
+   }
 }
